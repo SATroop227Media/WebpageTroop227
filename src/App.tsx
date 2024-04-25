9998b957c3961680d4483d1ccf0957236d8f7dec
Michael <moreno.m2698@gmail.com>
2024-04-25 17:21:41 -0400

    touched up mission statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import project1 from './assets/project1.jpg'
 import project2 from './assets/project2.jpg'
 import project3 from './assets/project3.jpg'
-import scoutMasters from './assets/leaders_edit.jpg'
+import group from './assets/group.jpg'
 import './App.css'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <header>
         <div className='header-logo'>
           <img src='https://beascout.scouting.org/wp-content/uploads/2022/06/BSALogo-1.png'/>
-          <h1 className='header-title'>Boy Scout Troop 227<span>Franklin Indiana</span></h1>
+          <h1 className='header-title'>Boy Scout Troop 227<span>American Legion Post 205</span></h1>
         </div>
         <nav>
           <ul>
@@ -27,21 +27,27 @@ function App() {
       </div>
     </section>
       <section className='home-intro'>
-        <div className='container'>
-          <figure>
-            <img src={scoutMasters}/>
-          </figure>
-          {/* This is copied from the offical BS site Make sure to change! */}
-          <div className='home-intro--content'>
-            <h1>Be Prepared</h1>
-            <h2 id='mission'>Our Mission</h2>
-            <p>The mission of the Boy Scouts of America is to prepare young people to make ethical and moral choices over their lifetimes by instilling in them the values of the Scout Oath and Law.</p>
-            <h2 id='scout-law'>Scout Law</h2>
-            <p>A Scout is trustworthy, loyal, helpful, friendly, courteous, kind, obedient, cheerful, thrifty, brave, clean, and reverent.</p>
-            <h2 id='scout-oath'>Scout Oath</h2>
-            <p>“On my honor I will do my best to do my duty to God and my country and to obey the Scout Law; to help other people at all times; to keep myself physically strong, mentally awake, and morally straight.”</p>  
-          </div>
-        
+        {/* This is copied from the offical BS site Make sure to change! */}
+        <div className='container home-intro--all'>
+          <h1>Be Prepared</h1>
+
+            <div className='home-intro--content'>
+              <div className='mission'>
+                <h2>Our Mission</h2>
+                <p>The mission of the Boy Scouts of America is to prepare young people to make ethical and moral choices over their lifetimes by instilling in them the values of the Scout Oath and Law.</p>
+              </div>
+              <div className='scout-law'>
+                <h2>Scout Law</h2>
+                <p>A Scout is trustworthy, loyal, helpful, friendly, courteous, kind, obedient, cheerful, thrifty, brave, clean, and reverent.</p>
+              </div>
+              <div className='scout-oath'>
+                <h2 id='scout-oath'>Scout Oath</h2>
+                <p>“On my honor I will do my best to do my duty to God and my country and to obey the Scout Law; to help other people at all times; to keep myself physically strong, mentally awake, and morally straight.”</p>  
+              </div>
+            </div>
+            <figure>
+              <img src={group}/>
+            </figure>
         </div>
       </section>
     <section className='home-gallary'>
