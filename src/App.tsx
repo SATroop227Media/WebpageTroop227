11cd0fea7a30f0fe8721b4b661b61f76da2b7482
troop227 <thetaegeukbonsai@gmail.com>
2024-05-02 17:29:41 -0400

    fixed some aria-labels

--- src/App.tsx
+++ src/App.tsx
@@ -32,59 +32,57 @@ function App() {
       </div>
     </section>
     </div>
-      <section className='home-intro' aria-labelledby="be-prepared">
+      <section className='home-intro'>
         {/* This is copied from the offical BS site Make sure to change! */}
         <div className='container home-intro--all'>
-          <h1 id='be-prepared'>Be Prepared</h1>
+          <h1>Be Prepared</h1>
             <div className='home-intro--content'>
-              <div className='mission' aria-labelledby='mission-header'>
-                <h2 className='home-intro--header' id='mission-header'>Scout Mission</h2>
+              <div className='mission'>
+                <h2 className='home-intro--header'>Scout Mission</h2>
                 <p className='home-intro--p'>The mission of the Boy Scouts of America is to prepare young people to make ethical and moral choices over their lifetimes by instilling in them the values of the Scout Oath and Law.</p>
               </div>
-              <div className='scout-law' aria-labelledby='scout-law-h'>
-                <h2 className='home-intro--header' id='scout-law-h'>Scout Law</h2>
+              <div className='scout-law'>
+                <h2 className='home-intro--header'>Scout Law</h2>
                 <p className='home-intro--p'>A Scout is trustworthy, loyal, helpful, friendly, courteous, kind, obedient, cheerful, thrifty, brave, clean, and reverent.</p>
               </div>
-              <div className='scout-oath' aria-labelledby='scout-oath-h'>
-                <h2 className='home-intro--header' id='scout-oath-h'>Scout Oath</h2>
+              <div className='scout-oath'>
+                <h2 className='home-intro--header'>Scout Oath</h2>
                 <p className='home-intro--p'>“On my honor I will do my best to do my duty to God and my country and to obey the Scout Law; to help other people at all times; to keep myself physically strong, mentally awake, and morally straight.”</p>  
               </div>
             </div>
-            <figure>
-              <div className='polaroid'>
-                <img src={group} alt='A group photo of Troop 227 at a camp site'/>
-              </div>
-            </figure>
+            <div className='polaroid'>
+              <img src={group} alt='A group photo of Troop 227 at a camp site'/>
+            </div>
         </div>
       </section>
-    <section className='home-impact' aria-labelledby='impact-h'>
-        <h1 id='impact-h'>Making a difference in the community</h1>
+    <section className='home-impact'>
+        <h1>Making a difference in the community</h1>
         <ul className='gallery responsive-grid' aria-label="community projects by troop 227">
           <li>
-            <figure className='gallery-item'>
+            <figure className='gallery-item' aria-labelledby='project-1'>
               <img src={project1} alt="Troop 227 scout and adult leaders measuring dimensions of a children's playground"/>
               <figcaption className='gallery-desc'>
-                  <h3>Project title</h3>
+                  <h3 id='project-1'>Project 1</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
                   <a href='' className='gallery-button'>Read More</a>
               </figcaption>
             </figure>
           </li>
           <li>
-            <figure className='gallery-item'>
+            <figure className='gallery-item' aria-labelledby='project-2'>
               <img src={project2} alt='Scout and his grandfather standing in front of food that was donated for his Eagle project'/>
               <figcaption className='gallery-desc'>  
-                  <h3>Project title</h3>
+                  <h3 id='project-2'>Project 2</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
                   <a href='' className='gallery-button'>Read More</a>
               </figcaption >
             </figure>
           </li>
           <li>
-            <figure className='gallery-item'>
+            <figure className='gallery-item' aria-labelledby='project-3'>
               <img src={project3} alt='Scout standing infront of clothing racks created for his Eagle project'/>
               <figcaption className='gallery-desc'>
-                  <h3>Project title</h3>
+                  <h3 id='project-3'>Project 3</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
                   <a href='' className='gallery-button'>Read More</a>
               </figcaption>
