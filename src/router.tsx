82edcac93ca596737a28f43d3b458461d4cbeb3f
moreno-m2698 <moreno.m2698@gmail.com>
2024-07-18 19:27:09 -0400

    Added routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -15,13 +14,15 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       {
-        path: "team",
+        path: "projects",
         element: <Projects />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+function RouterFunction() {
+    return <RouterProvider router={router} />
+}
+
+export default RouterFunction
