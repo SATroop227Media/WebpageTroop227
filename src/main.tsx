ab02e4a114cb9b5ca2ae19aa5593eb65e50519ee
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-01 20:15:45 -0400

    css selectors for color

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import "./styles/global/index.css"
 import Router from './router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
