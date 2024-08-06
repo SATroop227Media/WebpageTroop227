ebdd8e91b038e8f62f7aad5f38712900f826b3b6
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-05 21:44:10 -0400

    Added scroll event listener

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState, UIEvent } from 'react';
+
 import { Link } from "react-router-dom"
 import "../styles/global/nav.css"
 
 
 export default function AppNav() {
 
+    //delay is a number, value is a generic
+
+    const [isHidden, setIsHidden] = useState<boolean>(false);
+    const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+    const handleScroll = () => {
+        const scrollTop = window.scrollY;
+        setScrollPosition(scrollTop)
+        console.log("Scroll Position")
+    }
+
+    const [debounceValue, setDebounceValue] = useState(isHidden);
+    const delay = 1000 / 60;
+
+    useEffect(() => {
+
+
+        
+        window.addEventListener('scroll',handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
+    
+
+    // useEffect(() => {
+    //     const timeout = setTimeout(() => {
+    //         setDebounceValue(isHidden);
+    //     }, delay);
+        
+    //     return () => clearTimeout(timeout);
+    // }, [isHidden, delay])
+
     return (
-        <header className="header-nav">
+        <header className={"header-nav".concat("", (isHidden ? " hide": ""))}>
             <div className='header-nav__logo' aria-roledescription="logo">
               <img src='https://beascout.scouting.org/wp-content/uploads/2022/06/BSALogo-1.png' alt="Official logo of the Boy Scouts of America"/>
               <h1 className='header-nav__title'>Boy Scout Troop 227<span>American Legion Post 205</span></h1>
@@ -22,5 +57,8 @@ export default function AppNav() {
             </nav>
         </header>
         
-    )  
+    )
+    
+    
+
 }
--- src/modules/main/pages/MainIndex.tsx
+++ src/modules/main/pages/MainIndex.tsx
@@ -1,11 +1,12 @@
-
-import {Outlet} from "react-router-dom"
+import { useState, useRef, useEffect } from "react"
+import { Outlet } from "react-router-dom"
 import AppNav from "../../../components/AppNav"
 import Footer from "../../../components/Footer"
 
 export default function MainPage() {
     return (
-        <>
+        <>  
+            {/* I think this is where I need to put the debouncer */}
             <AppNav />
             <Outlet />
             <Footer />
