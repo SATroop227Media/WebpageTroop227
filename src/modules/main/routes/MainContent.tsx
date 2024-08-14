a7d587e4f67b73be850529b6df562a25208b7d60
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-14 18:13:31 -0400

    added intro section

--- src/modules/main/routes/MainContent.tsx
+++ src/modules/main/routes/MainContent.tsx
@@ -3,9 +3,11 @@ import project2 from '../../../assets/project2.jpg'
 import project3 from '../../../assets/project3.jpg'
 import group from '../../../assets/group.jpg'
 import stock1 from '../../../assets/Stock1.jpg'
+import eagle2 from '../../../assets/EagleScout2.jpg'
 
 import '../../../styles/global/App.css'
 import '../../../styles/components/mainhero.css'
+import '../../../styles/components/mainintro.css'
 import '../../../styles/components/mainimpact.css'
 import '../../../styles/components/maincontent.css'
 
@@ -22,47 +24,26 @@ function MainContent() {
           </div>
         </section>
 
-        <section className='home-impact' aria-label='making a difference'>
-          <h2>See our Impact!</h2>
-          <p>Troop 227 aids in community development by volunteering and by giving youth a safe space to grow and foster their values. 
-            Across Johnson County you can find where our young leaders have left their mark through the love for their community and fpr scouting.
+        <section className='home-intro'>
+          <h2>Come be a part of Troop 227!</h2>
+          <p>
+            Troop 227 began when it was chartered in __ and has been serving scouts in the Johnson County area ever since. We are a part of the Pathfinder District within the Indiana's Cross Roads of America Council. 
           </p>
-          <ul className='gallery responsive-grid' aria-label="community projects by troop 227">
-            <li>
-              <figure className='gallery-item' aria-labelledby='project-1'>
-                <img src={project1} alt="Troop 227 scout and adult leaders measuring dimensions of a children's playground"/>
-                <figcaption className='gallery-desc'>
-                    <h3 id='project-1'>Project 1</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
-                    <a href='' className='gallery-button'>Read More</a>
-                </figcaption>
-              </figure>
-            </li>
-            <li>
-              <figure className='gallery-item' aria-labelledby='project-2'>
-                <img src={project2} alt='Scout and his grandfather standing in front of food that was donated for his Eagle project'/>
-                <figcaption className='gallery-desc'>  
-                    <h3 id='project-2'>Project 2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
-                    <a href='' className='gallery-button'>Read More</a>
-                </figcaption >
-              </figure>
-            </li>
-            <li>
-              <figure className='gallery-item' aria-labelledby='project-3'>
-                <img src={project3} alt='Scout standing infront of clothing racks created for his Eagle project'/>
-                <figcaption className='gallery-desc'>
-                    <h3 id='project-3'>Project 3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
-                    <a href='' className='gallery-button'>Read More</a>
-                </figcaption>
-              </figure>
-            </li>
-          </ul>
+          <p>
+            Our mission is to foster within our scouts the vision of Scouting and inspire them to develop as leaders within their community. 
+            By living life aligned with the values of the Scout Oath and Scout Law, our scouts carry on the wishes of Scouting's founder Lord Baden-Powell while also developing their character. 
+            We want to create in our troop a community that encourages brotherhood while also allowing the fun that can be found in scouting to be at the forefront.
+          </p>
+          <p>
+            We sincerely hope that you and your family consider becoming involved with Scouting and that you contact one of our leaders if you're interested in joining.
+          </p>
         </section>
 
         <section className='home-content' aria-label="Be prepared">
           {/* This is copied from the offical BS site Make sure to change! */}
+          <div>
+            <img src={eagle2}/>
+          </div>
           <div className='container home-intro--all'>
             <h1>Be Prepared</h1>
               <div className='home-intro--content'>
@@ -83,6 +64,42 @@ function MainContent() {
                 <img src={group} alt='A group photo of Troop 227 at a camp site'/>
               </div>
           </div>
+        </section>
+
+        <section className='home-impact' aria-label='making a difference'>
+          <h2>See our Impact!</h2>
+          <p>Troop 227 aids in community development by volunteering and by giving youth a safe space to grow and foster their values. 
+            Across Johnson County you can find where our young leaders have left their mark through the love for their community and fpr scouting.
+          </p>
+          <ul className='gallery responsive-grid' aria-label="community projects by troop 227">
+            <li>
+              <figure className='gallery-item' aria-labelledby='project-1'>
+                <img src={project1} alt="Troop 227 scout and adult leaders measuring dimensions of a children's playground"/>
+                <figcaption className='gallery-desc'>
+                    <h3 id='project-1'>Project 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
+                </figcaption>
+              </figure>
+            </li>
+            <li>
+              <figure className='gallery-item' aria-labelledby='project-2'>
+                <img src={project2} alt='Scout and his grandfather standing in front of food that was donated for his Eagle project'/>
+                <figcaption className='gallery-desc'>  
+                    <h3 id='project-2'>Project 2</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
+                </figcaption >
+              </figure>
+            </li>
+            <li>
+              <figure className='gallery-item' aria-labelledby='project-3'>
+                <img src={project3} alt='Scout standing infront of clothing racks created for his Eagle project'/>
+                <figcaption className='gallery-desc'>
+                    <h3 id='project-3'>Project 3</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer feugiat scelerisque varius morbi enim. Et pharetra pharetra massa massa ultricies mi quis hendrerit dolor. Mi sit amet mauris commodo quis. </p>
+                </figcaption>
+              </figure>
+            </li>
+          </ul>
         </section>
       </>
     )
