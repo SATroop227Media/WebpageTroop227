81b407e9fa2f7233489548412b7d386e889546c2
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-15 16:13:36 -0400

    made hero responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="cta">
           <div className='cta__left' aria-label='contact information'>
-            <h2 className="cta__h2">Contact Us!</h2>
+            <h4 className="cta__h4">Contact Us!</h4>
             <p>Email: mamoreno16gb@gmail.com</p>
             <p>Telephone: (317) 694-6384</p>
           </div>
