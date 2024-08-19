450750a40ba6b02015515de12132b722af74e498
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-19 14:43:06 -0400

    added interaction observer

--- src/modules/main/routes/MainContent.tsx
+++ src/modules/main/routes/MainContent.tsx
@@ -15,42 +15,38 @@ import { useEffect, useState, useRef } from 'react'
 
 function MainContent() {
 
-  const [valuesIsVisible, setValuesIsVisible] = useState(false);
-
-
-  const singleEntryObserver = new IntersectionObserver(entries => {
-    const [ entry ] = entries;
-    entry.target.classList.toggle("observed", entry.isIntersecting)
-    if (entry.isIntersecting) singleObserver.unobserve(entry.target)
-  })
-  
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1.0
-  }
-
-
   useEffect(() => {
     const animatables = document.querySelectorAll("[data-comp-observee]")
+
     console.log(animatables)
-    // const observer = new IntersectionObserver((entries: [any]) => {
-    //   const [ entry ] = entries
-    //   setValuesIsVisible(entry.isIntersecting)
-    //   if (entry.isIntersecting) observer.unobserve(entry)
-    // }, options)
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        entry.target.classList.toggle("observed", entry.isIntersecting);
+        if (entry.isIntersecting) observer.unobserve(entry.target);
+      });
+    }, 
+    {
+      root: null,
+      threshold: 1,
+      rootMargin: "0px"
+    });
 
-    // if (containerRef.current) observer.observe(containerRef.current)
+    animatables.forEach(animatable => {
+      observer.observe(animatable)
+    })
 
-    // return () => {
-    //   if (containerRef.current) observer.unobserve(containerRef.current)
-    // }
+    return () => {
+      animatables.forEach(animatable => {
+        observer.unobserve(animatable)
+      })
+    }
 
-  }, [options])
+
+  }, [])
 
   return (
     <>
-      <section className="home-hero observed" aria-label="Our Impact" data-comp-observee="once">
+      <section className="home-hero observed" aria-label="Our Impact" data-comp-observee="">
         <article className='home-hero__content'>
           <h1 className='home-hero__header'>Creating the leaders of tomorrow by fostering the youth of today  </h1>
           <p className='home-hero__description'>For over 60 years, Troop 227 has been active in Johnson County and bringing about positive change. We seek to instill the values and mission of Scouting into the young men and women by teaching skills and lessons to last a life time!</p>
@@ -86,15 +82,15 @@ function MainContent() {
             By teaching our scouts the importance of preparing themselves to become strong leaders and productive citizens we give them the skills that will last a lifetime.</p>
           <div className='home-values__content-bottom'>
             <div className='home-values__cards-content'>
-              <div className='home-values__card' data-comp-observee="once">
+              <div className='home-values__card' data-comp-observee="">
                 <h3>Scout Mission</h3>
                 <p>The mission of the Boy Scouts of America is to prepare young people to make ethical and moral choices over their lifetimes by instilling in them the values of the Scout Oath and Law.</p>
               </div>
-              <div className='home-values__card' data-comp-observee="once">
+              <div className='home-values__card' data-comp-observee="">
                 <h3>Scout Law</h3>
                 <p>A Scout is trustworthy, loyal, helpful, friendly, courteous, kind, obedient, cheerful, thrifty, brave, clean, and reverent.</p>
               </div>
-              <div className='home-values__card' data-comp-observee="once">
+              <div className='home-values__card' data-comp-observee="">
                 <h3>Scout Oath</h3>
                 <p>“On my honor I will do my best to do my duty to God and my country and to obey the Scout Law; to help other people at all times; to keep myself physically strong, mentally awake, and morally straight.”</p>  
               </div>
@@ -112,21 +108,21 @@ function MainContent() {
           Across Johnson County you can find where our young leaders have left their mark through the love for their community and fpr scouting.
         </p>
         <ul className='home-impact__list' aria-label="community projects by troop 227">
-          <li className=''>
+          <li className='' data-comp-observee="">
             <figure className='home-impact__list-item polaroid slide-right' aria-labelledby='project-1'>
               <img src={project1} alt="Troop 227 scout and adult leaders measuring dimensions of a children's playground"/>
               <figcaption className='gallery-desc'>
               </figcaption>
             </figure>
           </li>
-          <li>
+          <li data-comp-observee="">
             <figure className='home-impact__list-item polaroid slide-right' aria-labelledby='project-2'>
               <img src={project2} alt='Scout and his grandfather standing in front of food that was donated for his Eagle project'/>
               <figcaption className='gallery-desc'>  
               </figcaption >
             </figure>
           </li>
-          <li>
+          <li data-comp-observee="">
             <figure className='home-impact__list-item polaroid slide-right' aria-labelledby='project-3'>
               <img src={project3} alt='Scout standing infront of clothing racks created for his Eagle project'/>
               <figcaption className='gallery-desc'>
