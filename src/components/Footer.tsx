c917bcf3be856b48c101d2f0abcfb3f84eda6869
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-21 19:11:11 -0400

    cleaned up spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
 import "../styles/global/footer.css"
 
 export default function Footer() {
-    return (
-        <footer className="cta">
-          <div className='cta__left' aria-label='contact information'>
-            <h4 className="cta__h4">Contact Us!</h4>
-            <p>Email: mamoreno16gb@gmail.com</p>
-            <p>Telephone: (317) 694-6384</p>
-          </div>
-          <div className='cta__right' aria-label='call to action'>
-            <p>Troop 227-Indiana 2024 | Join us today!</p>
-          </div>
-        </footer>
-    )
+  return (
+    <footer className="cta">
+      <div className='cta__left' aria-label='contact information'>
+        <h4 className="cta__h4">Contact Us!</h4>
+        <p>Email: mamoreno16gb@gmail.com</p>
+        <p>Telephone: (317) 694-6384</p>
+      </div>
+      <div className='cta__right' aria-label='call to action'>
+        <p>Troop 227-Indiana 2024 | Join us today!</p>
+      </div>
+    </footer>
+  );
 }
