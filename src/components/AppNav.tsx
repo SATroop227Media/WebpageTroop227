b9e948298f014633bd19d8a6d5dc3fba5dd35886
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-21 19:10:26 -0400

    cleaned up spacing

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -4,70 +4,59 @@ import { Link } from "react-router-dom"
 import "../styles/global/nav.css"
 
 export default function AppNav() {
-    const [lastScrollTop, setLastScrollTop] = useState<number>(0);
-    const [scrollDirection, setScrollDirection] = useState<number>(0); // 'up' or 'down'
-    
+  const [lastScrollTop, setLastScrollTop] = useState<number>(0);
+  const [scrollDirection, setScrollDirection] = useState<number>(0);
 
-    //TODO: double check tomorrow
-    //debouner is non-responsive/it just disappears from page functionality
+  const eventDebounce = (cb: any, delay: number = 600 / 60) => {
+    let timeout = setTimeout(cb, delay); //Performs my function after a delay
 
-    const eventDebounce = (cb: any, delay: number = 600 / 60) => {
-        let timeout = setTimeout(cb, delay); //Performs my function after a delay
-
-        return () => {
-            clearTimeout(timeout); //clears out timeout variable
-        
-            timeout = setTimeout(() => { //reinstantiates a new callback to timeout var to complete cycle
-                cb();
-            }, timeout)
-        }
+    return () => {
+      clearTimeout(timeout); //clears out timeout variable
+      timeout = setTimeout(() => { //reinstantiates a new callback to timeout var to complete cycle
+        cb();
+      }, timeout);
     }
+  }
 
-    const handleScroll = (): void => {
+  const handleScroll = (): void => {
+    const scrollTop = window.scrollY; //returns number of pixels that the window is scrolled vertially
 
-        const scrollTop = window.scrollY; //returns number of pixels that the window is scrolled vertially
-    
-        if (scrollTop > lastScrollTop) {
-          setScrollDirection(-1);
-        } else if (scrollTop < lastScrollTop) {
-          setScrollDirection(1);
-        }
-        setLastScrollTop(scrollTop); //resets last scroll amount
-      };
-    
-      useEffect(() => {
-        const debounceFunction = eventDebounce(handleScroll)
-        window.addEventListener('scroll', debounceFunction);
+  if (scrollTop > lastScrollTop) {
+    setScrollDirection(-1); //down
+  } else if (scrollTop < lastScrollTop) {
+    setScrollDirection(1); //up
+  }
+  setLastScrollTop(scrollTop); //resets last scroll amount
+  };
+  
+  useEffect(() => {
+    const debounceFunction = eventDebounce(handleScroll);
 
+    window.addEventListener('scroll', debounceFunction);
 
-        return () => {
-          window.removeEventListener('scroll', debounceFunction);
-        };
-      }, [lastScrollTop]);
+    return () => {
+      window.removeEventListener('scroll', debounceFunction);
+    };
+  }, [lastScrollTop]);
 
-
-
-    return (
-        <header className={"troop227-header".concat(" ", scrollDirection >= 0 ? "" : "hide")}>
-            <div className='troop227-header__logo' aria-roledescription="logo">
-              <img src="https://www.scouting.org/wp-content/uploads/2022/06/bsa-logo-hero-1.png" alt="Boy Scout Logo White Transparent"/>
-              <h1 className='troop227-header__title' data-mobile-only="">Troop 227</h1>
-              <h1 className='troop227-header__title' data-desktop-only="">Boy Scout Troop 227<span>American Legion Post 205</span></h1>
-            </div>
-            <nav className='troop227-header__nav'>
-                <ul className='troop227-header__list'>
-                    <li className='troop227-header__list-item'>
-                        <Link className={"troop227-header__link"} to={`/`}>Home</Link>
-                    </li>
-                    <li>
-                        <Link className={"troop227-header__link"} to={`/projects`}>Projects</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        
-    )
-    
-    
+  return (
+    <header className={"troop227-header".concat(" ", scrollDirection >= 0 ? "" : "hide")}>
+      <div className='troop227-header__logo' aria-roledescription="logo">
+        <img src="https://www.scouting.org/wp-content/uploads/2022/06/bsa-logo-hero-1.png" alt="Boy Scout Logo White Transparent"/>
+        <h1 className='troop227-header__title' data-mobile-only="">Troop 227</h1>
+        <h1 className='troop227-header__title' data-desktop-only="">Boy Scout Troop 227<span>American Legion Post 205</span></h1>
+      </div>
+      <nav className='troop227-header__nav'>
+        <ul className='troop227-header__list'>
+          <li className='troop227-header__list-item'>
+            <Link className={"troop227-header__link"} to={`/`}>Home</Link>
+          </li>
+          <li>
+            <Link className={"troop227-header__link"} to={`/projects`}>Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </header> 
+  );
 }
 
