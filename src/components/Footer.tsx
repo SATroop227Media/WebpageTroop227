3a84732608c3988c304a98d4f8704d53d0f05381
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-25 16:43:53 -0400

    added a tags to contacts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,20 @@ import "../styles/global/footer.css"
 export default function Footer() {
   return (
     <footer className="cta">
-      <div className='cta__left' aria-label='contact information'>
-        <h4 className="cta__h4">Contact Us!</h4>
-        <p>Email: mamoreno16gb@gmail.com</p>
-        <p>Telephone: (317) 694-6384</p>
-      </div>
-      <div className='cta__right' aria-label='call to action'>
-        <p>Troop 227-Indiana 2024 | Join us today!</p>
-      </div>
+      <h3>Contact Us!</h3>
+      <ul className="cta__contact-wrapper">
+        <li>
+          <h4>Joseph "JD" Henry - Scoutmaster</h4>
+          <a href="mailto:Jdhenryx@gmail.com">Jdhenryx@gmail.com</a>
+          <a href="tel:3173139392">(317) 313-9392</a>
+        </li>
+        <li>
+          <h4>Ron Smith - Committee Chair</h4>
+          <a href="mailto:rsmith@indianaskypics.com">rsmith@indianaskypics.com</a>
+          <a href="tel:3174392640">Telephone: (317) 439-2640</a>
+        </li>
+      </ul>
+      <p>Troop 227-Indiana 2024 | Join us today!</p>
     </footer>
   );
 }
