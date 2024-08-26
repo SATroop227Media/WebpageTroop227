2368e9ed38a47780b6de5d033dc50bb9b9d2999e
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-25 21:12:48 -0400

    added projects content

--- src/modules/main/routes/ProjectContent.tsx
+++ src/modules/main/routes/ProjectContent.tsx
@@ -6,6 +6,220 @@ function ProjectContent() {
             <section className="project-hero">
                 <img src={eagle}/>
             </section>
+            <section className="project-content">
+                <ol className="project__year-list">
+                    <li>
+                        <h2>2003</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Rusty Leonard</h3>
+                                <p>Refurbished a basketball court area in Switzer Park, Bargersville, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2005</h2>
+                        <ul className="project__project-list">
+                            <h3>Bobby Leonard</h3>
+                            <p>Added playground equipment to Switzer Park, Bargersville, IN.</p>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2006</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Dalton Chin</h3>
+                                <p>Built an outdoor chapel at Praise Acres, including benches (pews), a podium, and a large cross.</p>
+                            </li>
+                            <li>
+                                <h3>Nick Leonard</h3>
+                                <p>Added a roof to Playground Equipment and a mulched trail at Switzer Park, Bargersville.</p>
+                            </li>
+                            <li>
+                                <h3>Paul Haney</h3>
+                                <p>Built a Marian Grotto at St. Rose of Lima Catholic Church in Franklin, IN.</p>                            
+                            </li>
+                            <li>
+                                <h3>Taylor Woodward</h3>
+                                <p>Rebuilt donated bicycles for Lord's Locker in Trafalgar.  Taylor's project was awarded the District and Council projects of the year.</p>
+                            </li>
+                            <li>
+                                <h3>Michael Kurek</h3>
+                                <p>Built and equipped a Science Lab at St. Rose School School in Franklin, IN, in memory of his father.</p>                            
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2007</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Matt Byrne</h3>
+                                <p>Built firepits at Praise Acres camping area.</p>
+                            </li>
+                            <li>
+                                <h3>John Bennett</h3>
+                                <p>Built a shelter, fireplace and chairs at Praise Acres camping area in Bargersville, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Brian Byrne</h3>
+                                <p>Built wooden toy cars for the Lord's Locker charity organization in Trafalgar, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2008</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Kevin Riley</h3>
+                                <p>Assembled book bags filled with school supplies for Johnson County and St. Rose of Lima Schools</p>
+                            </li>
+                            <li>
+                                <h3>Cody Magar</h3>
+                                <p>Rebuilt and painted shelter house at Hopewell Church in Bargersville, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2009</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Brady Aldridge</h3>
+                                <p>Built benches and picnic tables for St. Rose of Lima School, along with flower boxes and garden areas.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2010</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>James Brummett</h3>
+                                <p>Built St. Rose of Lima Church entry signs and concrete patio and kneelers for the Marian Grotto.</p>
+                            </li>
+                            <li>
+                                <h3>William Roush</h3>
+                                <p>Renovated the playground ground and constructed a front sign (including a light system for messages) for St. Rose of Lima Catholic School in Franklin, IN.</p>
+                            </li>
+                            <li>
+                                <h3>William Torrance</h3>
+                                <p>Constructed a large perrenial garden at Windisch Park in Bargersville, IN, which included the construction of pergolas.</p>
+                            </li>
+                            <li>
+                                <h3>Drew Baker</h3>
+                                <p>Built a handicap ramp, removed a garage, and donated supplies to women's shelter in Shelbyville, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Brian Adams</h3>
+                                <p>Built picnic tables and benches for Windisch Park in Bargersville, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2012</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Glenn Kyle III</h3>
+                                <p>Renovated gardens used for science classes at Union Elementary School in Bargersville, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Ryan Long</h3>
+                                <p>Renovated a jet plane display and grounds at American Legion in Edinburg, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2014</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Caleb Brown</h3>
+                                <p>Made Christmas trees using tomato cages and donated garland, lights, and ornaments for the Lord's Locker in Trafalgar, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2016</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Christian Kyle</h3>
+                                <p>Added statue, memorial plaque, and plantings at Windisch Park in Bargersville, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Michael Moreno</h3>
+                                <p>Renovated gardens at Torrance Eagle Project at Windisch Park in Bargersville, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Travis Davison</h3>
+                                <p>Built a handicap ramp for St. Rose of Lima School modular building allowing school to meet ADA requirments.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2017</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Jacob McKinney</h3>
+                                <p>Renovated Scott Park near baseball diamonds in Franklin, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Austin Hicks</h3>
+                                <p>Renovated and landscaped at the American Legion in Franklin, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2019</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Seth Long</h3>
+                                <p>Built playground equipment, including gaga pit, at St. Rose of Lima School in Franklin, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2020</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Aiden Miller</h3>
+                                <p>Gathered care packages for the Wheeler Mission Women's Shelter in Indianapolis, IN.</p>
+                            </li>
+                            <li>
+                                <h3>Gaven Miller</h3>
+                                <p>Built Pre-School playground equipment at St. Rose of Lima Catholic School in Franklin, IN.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2022</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Alejandro Garnica</h3>
+                                <p>Created weekend meals and birthday gifts for patrons of the Interchurch Food Pantry of Johnson County.</p>
+                            </li>
+                            <li>
+                                <h3>Joshua Stephan Miller</h3>
+                                <p>Helped Resources of Hope in Whiteland IN move their inventory to a new facility and built clothing shelves for their new location.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2023</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Damien Burke</h3>
+                                <p>Created Christmas Care Packages for the Franklin Senior Citizens Center, and planned and emceed a party for the seniors, at which the care packages were distributed.</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>2024</h2>
+                        <ul className="project__project-list">
+                            <li>
+                                <h3>Braiddinn Plymate</h3>
+                                <p>Built a Covid-19 Memorial at Franklin IN Park, honoring emergency workers, first responders, and those who died during the COVID-19 epidemic.  The memorial included a timecapsule with letters and articles discussing life during the epidemic. Braiddinn's project was awarded the District and Council project of the year.</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
+            </section>
         </>
     )
 }
