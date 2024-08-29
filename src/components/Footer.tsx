935a218563bd5bd3674bf807e780680ce9ba3eab
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-29 17:30:46 -0400

    styled footer'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,15 +8,15 @@ export default function Footer() {
         <li>
           <h4>Joseph "JD" Henry - Scoutmaster</h4>
           <a href="mailto:Jdhenryx@gmail.com?subject=Joining Troop 227">Email: Jdhenryx@gmail.com</a>
-          <a href="tel:3173139392">(317) 313-9392</a>
+          <a href="tel:3173139392">Phone: (317) 313-9392</a>
         </li>
         <li>
           <h4>Ron Smith - Committee Chair</h4>
           <a href="mailto:rsmith@indianaskypics.com?subject=Joining Troop 227">Email: rsmith@indianaskypics.com</a>
-          <a href="tel:3174392640">Telephone: (317) 439-2640</a>
+          <a href="tel:3174392640">Phone: (317) 439-2640</a>
         </li>
       </ul>
-      <p>Troop 227-Indiana 2024 | Join us today!</p>
+      <p>Boy Scout Troop 227 - Indiana 2024 | Join us today!</p>
     </footer>
   );
 }
