2695a13968de995be8d9a57fe9e96685f542c86e
moreno-m2698 <moreno.m2698@gmail.com>
2024-08-29 15:10:24 -0400

    accessbility changes for nav

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -6,16 +6,16 @@ export default function AppNav() {
   return (
     <header className="troop227-header">
       <div className='troop227-header__logo' aria-roledescription="logo">
-        <img src="https://www.scouting.org/wp-content/uploads/2022/06/bsa-logo-hero-1.png" alt="Boy Scout Logo White Transparent"/>
+        <img src="https://www.scouting.org/wp-content/uploads/2022/06/bsa-logo-hero-1.png" alt="Boy Scout Logo White Transparent" aria-hidden="true" />
         <h1 className='troop227-header__title' data-mobile-only="">Troop 227</h1>
         <h1 className='troop227-header__title' data-desktop-only="">Boy Scout Troop 227<span>American Legion Post 205</span></h1>
       </div>
-      <nav className='troop227-header__nav'>
+      <nav className='troop227-header__nav' aria-label="Site Navigation">
         <ul className='troop227-header__list'>
           <li className='troop227-header__list-item'>
             <Link className={"troop227-header__link"} to={`/`}>Home</Link>
           </li>
-          <li>
+          <li className='troop227-header__list-item'>
             <Link className={"troop227-header__link"} to={`/projects`}>Projects</Link>
           </li>
         </ul>
